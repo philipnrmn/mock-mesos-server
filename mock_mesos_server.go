ee4609ef016d044f97ec79b4b6d45241f7035441
Philip Norman <philip@mesosphere.io>
2018-12-04 15:13:25 -0800

    Add GET_TASKS

--- mock_mesos_server.go
+++ mock_mesos_server.go
@@ -15,9 +15,11 @@ import (
 )
 
 // raw protobuf request types:
+// ref https://github.com/apache/mesos/blob/master/include/mesos/v1/agent/agent.proto
 var (
 	GET_CONTAINERS = []byte{8, 10}
 	GET_STATE      = []byte{8, 9}
+	GET_TASKS      = []byte{8, 13}
 )
 
 // startTestServer starts a server and serves the specified fixture's content
@@ -37,6 +39,11 @@ func startTestServer(t *testing.T, fixture string) *httptest.Server {
 		if bytes.Equal(body, GET_STATE) {
 			state := loadFixture(t, filepath.Join(fixture, "state.bin"))
 			w.Write(state)
+			return
+		}
+		if bytes.Equal(body, GET_TASKS) {
+			tasks := loadFixture(t, filepath.Join(fixture, "tasks.bin"))
+			w.Write(tasks)
 			return
 		}
 		t.Fatal("Body contained an unknown request: ", body)
